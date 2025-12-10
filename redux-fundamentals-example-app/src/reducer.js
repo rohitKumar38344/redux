f8068c1ae0faf93b554bd2364389063973c1b34b
rohit kumar <rohitkumar38344@gmail.com>
2025-12-11 00:14:19 +0530

    feat: add appReducer function

--- redux-fundamentals-example-app/src/reducer.js
+++ redux-fundamentals-example-app/src/reducer.js
@@ -34,11 +34,49 @@ const initialState = {
   },
 };
 
-export default function appReducer(state = initialState, action) {
-  switch (key) {
-    case value:
-      break;
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+  return maxId + 1;
+}
 
+export default function appReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'todos/todoAdded':
+      return {
+        ...state,
+
+        todos: [
+          ...state.todos,
+          {
+            id: nextTodoId(),
+            text: action.payload,
+            completed: false,
+          },
+        ],
+      };
+    case 'todos/todoToggled':
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id === payload.id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            };
+          } else {
+            return todo;
+          }
+        }),
+      };
+    case 'filters/statusFilterChanged':
+      return {
+        ...state,
+        todos: [...state.todos],
+        filters: {
+          ...state.filters,
+          filters: action.payload,
+        },
+      };
     default:
       return state;
   }
