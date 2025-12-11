82330d7030b7468c97912c19b2b92eb9dd0a678f
rohit kumar <rohitkumar38344@gmail.com>
2025-12-11 16:00:52 +0530

    feat: add combineReducer

--- redux-fundamentals-example-app/src/reducer.js
+++ redux-fundamentals-example-app/src/reducer.js
@@ -1,9 +1,10 @@
+import { combineReducers } from 'redux';
 import todosReducer from './features/todos/todosSlice';
 import filtersReducer from './features/filters/filtersSlice';
 
-export default function appReducer(state = {}, action) {
-  return {
-    todos: todosReducer(state.todos, action),
-    filters: filtersReducer(state.filters, action),
-  };
-}
+const rootReducer = combineReducers({
+  todos: todosReducer(state.todos, action),
+  filters: filtersReducer(state.filters, action),
+});
+
+export default rootReducer;
